ad1175849ad397149eb0323a38be5281d5c32c95
felipefoster <88387736+felipefoster@users.noreply.github.com>
2021-10-21 18:04:34 -0300

    Finalizado
    Finalizado

--- index.js
+++ index.js
@@ -19,46 +19,44 @@ const map = [
 
 
 function criaMap() {
+   
     const container = document.createElement('div')
     container.classList.add("container")
     document.body.appendChild(container)
     for(let i = 0; i < map.length; i++){
         const linhaMap = document.createElement("div")
         linhaMap.classList.add("linhaMap")
+        linhaMap.setAttribute('id',i)        
         container.appendChild(linhaMap)
         for(let j = 0; j < map[i].length; j++){
             const celulaMap = document.createElement("div")
             celulaMap.classList.add("celulaMap")
+            celulaMap.classList.add(i+'-'+j)
             linhaMap.appendChild(celulaMap)
+           
             if(map[i][j] === 'W'){
-                const w = document.createElement('p')
-                w.classList.add('parede')
-                const text = document.createTextNode(' ')
-                celulaMap.appendChild(w)
-                w.appendChild(text)
-            } else if (map[i][j] === ' '){
-                const espaco = document.createElement('p')
-                const textespaco = document.createTextNode(' ')
-                celulaMap.appendChild(espaco)
-                espaco.appendChild(textespaco)
-            } else if (map[i][j] === 'S'){
-                const s = document.createElement('p')
+                celulaMap.classList.add("celulaMapW")
+                const block = document.createElement("div")
+                celulaMap.appendChild(block)
+
+            } 
+            else if (map[i][j] === ' '){
+                celulaMap.classList.add("celulaMapEspaco")
+            } 
+            else if (map[i][j] === 'S'){
                 const bola = document.createElement("div")
                 bola.classList.add('bola')
-                linhaMap.appendChild(bola)
-                const texts = document.createTextNode('S')
-                celulaMap.appendChild(s)
-                s.appendChild(texts)
-            } else if (map[i][j] === 'F'){
-                const f = document.createElement('p')
-                const textf = document.createTextNode('F')
-                celulaMap.appendChild(f)
-                f.appendChild(textf)
-            } else if (map[i][j] === ' '){
-                const espaco = document.createElement('p')
-                const textespaco = document.createTextNode(' ')
-                celulaMap.appendChild(espaco)
-                espaco.appendChild(textespaco)
+                bola.setAttribute('id','moveBola')
+                celulaMap.appendChild(bola)
+                celulaMap.classList.add(i+'-'+j)
+                
+            } 
+            else if (map[i][j] === 'F'){
+                celulaMap.classList.add(i+'-'+j)
+            } 
+            else if (map[i][j] === ' '){
+                celulaMap.classList.add(i+'-'+j)
+                celulaMap.classList.add("celulaMapEspaco")
             }
         }
     }
@@ -66,9 +64,61 @@ function criaMap() {
 
 
 criaMap()
+    
 
+let celulas = document.getElementsByClassName('celulaMap')
+let linhas = document.getElementsByClassName('linhaMap')
+const pegaBola = document.getElementById('moveBola')
+
+
+
+    let i = 9
+    let j = 0
+    
+    
 document.addEventListener('keydown', function(event) {
     const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    
+    if (key === 'ArrowRight'){
+        console.log('direita')
+        if (linhas[i].children[j+1].className.includes('celulaMapW') !== true){
+        linhas[i].children[j+1].appendChild(pegaBola) 
+        j += 1
+        win ()
+    }
+    } else if (key === "ArrowLeft"){
+        console.log('esquerda')
+        if (linhas[i].children[j-1].className.includes('celulaMapW') !== true){
+        linhas[i].children[j-1].appendChild(pegaBola)
+        j -= 1
+        win ()
+        }
+    } else if (key === "ArrowUp"){
+        console.log('cima')
+        if (linhas[i-1].children[j].className.includes('celulaMapW') !== true){
+        linhas[i-1].children[j].appendChild(pegaBola)
+        i -= 1
+        win ()
+        }
+    } else if (key === "ArrowDown"){
+        console.log('baixo')
+        if (linhas[i+1].children[j].className.includes('celulaMapW') !== true){
+        linhas[i+1].children[j].appendChild(pegaBola)
+        i += 1
+        
+        }
+        win ()
+    }
+    
 });
 
-let moveBola = document.getElementsByClassName('bola')
+
+function win (){
+    const alerta = document.querySelector('.pop-teste')
+    if(pegaBola.parentElement.className === 'celulaMap 8-20'){
+        ('Vitória')
+        alerta.style.display = 'block';
+    }
+}
+
+let botao = document.getElementsByClassName('botao')
